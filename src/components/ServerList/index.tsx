60cf35fa3c47a74d35264ca1eee96055898b9910
Guilherme <guilherme.gl1997@gmail.com>
2020-06-24 19:13:40 -0300

    first commit

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ServerList: React.FC = () => {
+    return (
+        <Container>
+            <ServerButton isHome />
+
+            <Separator />
+
+            <ServerButton />
+            <ServerButton hasNotifications />
+            <ServerButton mentions={3} />
+            <ServerButton />
+            <ServerButton />
+            <ServerButton />
+            <ServerButton />
+            <ServerButton />
+            <ServerButton />
+            <ServerButton />
+            <ServerButton />
+        </Container>
+    );
+};
+
+export default ServerList;
